08002f1b97f3bed19bda16b88af366b4626b2853
kseniyakruchina@gmail.com <kseniyakruchina@gmail.com>
2024-02-08 11:16:28 +0600

    SORRY TRUE HOMEWORK

--- homework_2/shop/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from .views import get_index_page, get_product_details
-
-urlpatterns = [
-    path('', get_index_page),
-    path('<int:pk>', get_product_details)
-]
